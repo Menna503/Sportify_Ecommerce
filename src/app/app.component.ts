1d81ce65bf4af7398a65245542bd0dc31bb7feef
samaahmedmohameds <sa6814620@gmail.com>
2025-02-22 16:10:23 +0200

    signupPart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from "./pages/home/home.component";
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent ,SignupPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
