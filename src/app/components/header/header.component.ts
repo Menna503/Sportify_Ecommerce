1cc23bdaba98e9ed0ce23be35ba58292a80fd47f
Menna503 <mennaallah503@gmail.com>
2025-02-28 15:02:00 +0200

    home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  show:string='hidden'
+  toggel() {
+    this.show = this.show === 'hidden' ? 'block' : 'hidden';
+  }
+  
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { RunningManComponent } from "../../components/running-man/running-man.component";
+import { CategoriesComponent } from "../../components/categories/categories.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RunningManComponent, CategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
