f268f64183aa2a540c5d5b112666b5342a821911
samaahmedmohameds <sa6814620@gmail.com>
2025-03-01 12:39:53 +0200

    signinup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from "./pages/home/home.component";
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent ,SignupPageComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent ,SignupPageComponent ,SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -3,25 +3,23 @@ import { CommonModule } from '@angular/common';
 import { FormGroup, FormBuilder, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup-page',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule], 
-  templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
+  selector: 'app-signin',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.css'
 })
-export class SignupPageComponent {
-  signUpPage = new FormGroup({
-    firstName: new FormControl('', [Validators.minLength(3), Validators.required]),
-    lastName: new FormControl('', [Validators.minLength(3), Validators.required]),
+export class SigninComponent {
+  signInPage=new FormGroup({
+    // firstName: new FormControl('', [Validators.minLength(3), Validators.required]),
+    // lastName: new FormControl('', [Validators.minLength(3), Validators.required]),
     email: new FormControl('', [Validators.email, Validators.required]),
     password: new FormControl('', [Validators.minLength(6), Validators.required]),
     rememberMe: new FormControl(false) 
   });
-
   submitted = false;  
 
   onSubmit() {
-    if(this.signUpPage.valid){
+    if(this.signInPage.valid){
       console.log("done");
     }else{
       console.log("empty");
