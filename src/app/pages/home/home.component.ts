d883ea7ef99782d23fb333cc9dd7e2ddad368310
Menna503 <mennaallah503@gmail.com>
2025-03-04 22:59:18 +0200

    filter

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { RunningManComponent } from "../../components/running-man/running-man.component";
 import { CategoriesComponent } from "../../components/categories/categories.component";
+import { MenCollectionComponent } from '../../components/image-collection/men-collection.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, RunningManComponent, CategoriesComponent],
+  imports: [HeaderComponent, RunningManComponent, CategoriesComponent,MenCollectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/pages/men/men.component.ts
+++ src/app/pages/men/men.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { MenCollectionComponent } from '../../components/image-collection/men-collection.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-men',
-  imports: [MenCollectionComponent,HeaderComponent,ProductCardComponent],
+  imports: [MenCollectionComponent,HeaderComponent,ProductCardComponent,CommonModule],
   templateUrl: './men.component.html',
   styleUrl: './men.component.css'
 })
 export class MenComponent {
-src="assets/images/men_collection.svg"
+src="assets/images/men_collection.svg";
+isHidden:boolean=true;
+toggelFilter(){
+  this.isHidden=!this.isHidden;
+}
 }
