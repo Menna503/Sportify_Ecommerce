accfba7f768570d3fe05645483a0ea83d13eb8e6
samaahmedmohameds <sa6814620@gmail.com>
2025-03-06 12:14:37 +0200

    routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FavComponent } from './pages/fav/fav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenComponent } from './pages/men/men.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SuplementsComponent } from './pages/suplements/suplements.component';
+import { WomenComponent } from './pages/women/women.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home' , component:HomeComponent},
+    {path:'signin' ,component:SigninComponent},
+    {path:'signup' ,component:SignupPageComponent},
+    {path:'fav' ,component:FavComponent},
+    {path:'men' ,component:MenComponent},
+    {path:'women' ,component:WomenComponent},
+    {path:'suplements' ,component:SuplementsComponent},
+];
