a1263cf60abbdc437a7fcf309b943d8f57435c01
Menna503 <mennaallah503@gmail.com>
2025-03-24 15:08:46 +0200

    improve filter

--- src/app/pages/men/men.component.ts
+++ src/app/pages/men/men.component.ts
@@ -7,9 +7,6 @@ import { PaginationComponent } from '../../components/pagination/pagination.comp
 import { FooterComponent } from "../../components/footer/footer.component";
 import { ProductService } from '../../services/products/product.service';
 import { HttpClient } from '@angular/common/http';
-import { flush } from '@angular/core/testing';
-
-
 
 @Component({
   selector: 'app-men',
@@ -18,275 +15,123 @@ import { flush } from '@angular/core/testing';
   styleUrl: './men.component.css'
 })
 export class MenComponent {
-src="assets/images/men_collection.svg";
-isHidden:boolean=true;
-hightChecked:boolean=true;
-lowChecked:boolean=true;
-adidas:boolean=true;
-nike:boolean=true;
-nilton:boolean=true;
-misery:boolean=true;
-hideBrand:boolean=true;
-hideBrice:boolean=false;
-selectedButton: string = 'price';
- menClothes:any
- subCategory:string='';
- brand:string='';
+  src = "assets/images/men_collection.svg";
+  isHidden: boolean = true;
+  hightChecked: boolean = true;
+  lowChecked: boolean = true;
+  adidas: boolean = true;
+  nike: boolean = true;
+  nilton: boolean = true;
+  misery: boolean = true;
+  hideBrand: boolean = true;
+  hideBrice: boolean = false;
+  selectedButton: string = 'price';
+  menClothes: any;
+  subCategory: string = '';
+  brand: string = '';
+  sort: string='';
 
-constructor(private http:HttpClient,private productService:ProductService){
-  
-}
+  constructor(private http: HttpClient, private productService: ProductService) {}
 
-ngOnInit (){
-  this.fetchData();
-}
+  ngOnInit() {
+    this.fetchData();
+  }
 
-fetchData(param:any={}){
-  this.productService.getProduct({gender:'men',category:'clothes',...param}) .subscribe(products => {
-    this.menClothes=products;
-    console.log(products);
-  });
-}
+  fetchData() {
+    const params = {
+      gender: 'men',
+      category: 'clothes',
+      subCategory: this.subCategory,
+      sort: this.sort,
+      brand: this.brand
+    };
 
-toggelFilter(){
-  this.isHidden=!this.isHidden;
-  // this.hightChecked=true;
-  // this.lowChecked=true;
- 
-}
+    // Remove keys with empty values
+    const filteredParams = Object.fromEntries(
+      Object.entries(params).filter(([_, value]) => value !== undefined && value !== "")
+    );
 
-showCheck(text:string){
-
-  switch(text){
-    case'hight':
-    this.hightChecked=false;
-    this.lowChecked=true;
-    if(this.subCategory.trim()){
-      if(this.brand.trim()){
-        // this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'-price',brand:this.brand}) .subscribe(products => {
-        //   this.menClothes=products;
-        //   console.log(products);
-        // });
-       this.fetchData({subCategory:this.subCategory,sort:'-price',brand:this.brand});
-       console.log(`sort:hight subcategory ${this.subCategory} brand: ${this.brand} `,this.menClothes);
-      }
-      else{
-        // this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'-price'}) .subscribe(products => {
-        //   this.menClothes=products;
-        //   console.log(products);
-        // });
-        this.fetchData({subCategory:this.subCategory,sort:'-price'});
-        console.log(`sort:hight subcategory: ${this.subCategory}`,this.menClothes);
-      }
-     
-    }
-    else if (this.brand.trim()){
-      this.productService.getProduct({gender:'men',category:'clothes',sort:'-price',brand:this.brand}) .subscribe(products => {
-        this.menClothes=products;
-        console.log(products);
-      });
-    }
-    else{
-      this.productService.getProduct({gender:'men',category:'clothes',sort:'-price'}) .subscribe(products => {
-        this.menClothes=products;
-        console.log(products);
-      });
-    }
-   
-    break;
-    case'low':
-    this.lowChecked=false;
-    this.hightChecked=true;
-    if(this.subCategory.trim()){
-      if(this.brand.trim()){
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'price',brand:this.brand}) .subscribe(products => {
-          this.menClothes=products;
+    // Ensure there are additional filters before sending the request
+    if (Object.keys(filteredParams).length >= 2) {
+      this.productService.getProduct(filteredParams)
+        .subscribe(products => {
+          this.menClothes = products;
           console.log(products);
         });
-      }
-      else{
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'price'}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-     
+    } else {
+      console.warn("No filters applied, request not sent!");
     }
-    else if (this.brand.trim()){
-      this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'price',brand:this.brand}) .subscribe(products => {
-        this.menClothes=products;
-        console.log(products);
-      });
-    }
-    else{
-      this.productService.getProduct({gender:'men',category:'clothes',sort:'price'}) .subscribe(products => {
-        this.menClothes=products;
-        console.log(products);
-      });
-    }
-    break;
-    case'Adidas':
-    this.adidas=false;
-    this.nike=true;
-    this.nilton=true;
-    this.misery=true;
-    this.brand='Adidas'; 
-    if(this.subCategory.trim()){
-      if(this.lowChecked==false){
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'price',brand:this.brand}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-      else if(this.hightChecked==false){
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,sort:'-price',brand:this.brand}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-      else{
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:this.subCategory,brand:this.brand}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-    }
-    else if(this.hightChecked==false){
-      this.productService.getProduct({gender:'men',category:'clothes',brand:this.brand,sort:'-price'}) .subscribe(products => {
-        this.menClothes=products;
-        console.log(products);
-      });
-    }
-    else if(this.lowChecked==false){
-      this.productService.getProduct({gender:'men',category:'clothes',brand:this.brand,sort:'price'}) .subscribe(products => {
-        this.menClothes=products;
-        console.log(products);
-      });
-    }
-    else{this.productService.getProduct({gender:'men',category:'clothes',brand:this.brand}) .subscribe(products => {
-      this.menClothes=products;
-      console.log("adid",products);
-    });}
-    
-    break;
-    case 'Nike':
-      this.nike=false;
-      this.adidas=true;
-      this.nilton=true;
-     this.misery=true;
-      this.brand='Nike'
-    break;
-    case 'Nileton':
-      this.nilton=false;
-      this.nike=true;
-      this.adidas=true;
-      this.misery=true;
-      this.brand='Nileton';
-    break;
-    case 'Mesery':
-      this.misery=false;
+  }
+
+  toggelFilter() {
+    this.isHidden = !this.isHidden;
+  }
+
+  showCheck(text: string) {
+    switch (text) {
+      case 'hight':
+        this.hightChecked = false;
+        this.lowChecked = true;
+        this.sort='-price';
+        break;
+      case 'low':
+        this.lowChecked = false;
+        this.hightChecked = true;
+        this.sort='price';
+        break;
+      case 'Adidas':
+        this.adidas=false;
+        this.nike=true;
+        this.nilton=true;
+        this.misery=true;
+        this.brand='Adidas'; 
+        break;
+      case 'Nike':
+        this.nike=false;
+        this.adidas=true;
+        this.nilton=true;
+        this.misery=true;
+        this.brand='Nike'
+        break;
+      case 'Nileton':
+        this.nilton=false;
+        this.nike=true;
+        this.adidas=true;
+        this.misery=true;
+        this.brand='Nileton';
+        break;
+      case 'Mesery':
+        this.misery=false;
       this.nilton=true;
       this.nike=true;
       this.adidas=true;
       this.brand='Mesery';
-    break;
-
-
+        break;
+    }
+    this.fetchData();
   }
- 
-}
 
- 
-display(text:string){
-  this.subCategory=text;
-       if(this.brand.trim()){
-        if(this.hightChecked===false){
-          this.productService.getProduct({gender:'men',category:'clothes',subCategory:text,sort:'-price',brand:this.brand}) .subscribe(products => {
-            this.menClothes=products;
-            console.log(products);
-          });
-        }
-        else if(this.lowChecked===false){
-          this.productService.getProduct({gender:'men',category:'clothes',subCategory:text,sort:'price',brand:this.brand}) .subscribe(products => {
-            this.menClothes=products;
-            console.log(products);
-          });
-        }
-       }
-     
-      else if(this.hightChecked===false){
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:text,sort:'-price'}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-      else if(this.lowChecked===false){
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:text,sort:'price'}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-      else{
-        this.productService.getProduct({gender:'men',category:'clothes',subCategory:text}) .subscribe(products => {
-          this.menClothes=products;
-          console.log(products);
-        });
-      }
-
- 
-}
-show(text:string){
-  this.selectedButton=text;
-  switch(text){
-    case 'brand':
-      this.hideBrand=false;
-      this.hideBrice=true;
-    
-      break;
-    case 'price':
-      this.hideBrice=false;
-      this.hideBrand=true;  
-    
+  display(text: string) {
+    this.subCategory = text;
+    this.fetchData();
   }
- 
+
+  show(text: string) {
+    this.selectedButton = text;
+    this.hideBrand = text !== 'brand';
+    this.hideBrice = text !== 'price';
+  }
+
+  itemsPerPage = 8;
+  currentPage = 1;
+
+  get paginatedData() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    return this.menClothes ? this.menClothes.slice(start, end) : [];
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+  }
 }
-
-
-itemsPerPage=8
-currentPage=1
-// data=[
-//   {id:1,},
-//   {id:2,},
-//   {id:3,},
-//   {id:4,},
-//   {id:5,},
-//   {id:6,},
-//   {id:7,},
-//   {id:8,},
-//   {id:9,},
-//   {id:10,},
-//   {id:11,},
-//   {id:12,},
-//   {id:14,},
-//   {id:15,},
-//   {id:16,},
-//   {id:17,},
-//   {id:18,},
-//   {id:19,},
-//   {id:20,},
-// ]
-
-get paginatedData(){
-  const start =(this.currentPage-1)*this.itemsPerPage;
-  const end = start +this.itemsPerPage
-
-  return this.menClothes.slice(start , end)
-}
-
-changePage(page :number){
-  this.currentPage=page
-}
-
-
-}
- 
