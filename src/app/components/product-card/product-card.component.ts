687030ed329083ba3f335832bcd3ed171555ce6a
ghada <ghadaelsayed20@gmail.com>
2025-04-04 17:33:56 +0200

    fav

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,21 +1,40 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, EventEmitter, Input,Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoritesService } from '../../services/favorites/favorites.service';
 
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,HeaderComponent,FooterComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
   @Input() data:any
   @Input() isFav:boolean=false;
+  @Output() removedFromFavorites = new EventEmitter<string>();
    
     constructor(private favoritesService:FavoritesService){}
+
+    ngOnInit(){
+      this.checkIfFavorite();
+    }
+
+    checkIfFavorite(){
+      this.favoritesService.getfavourite().subscribe({
+        next:(favourits)=>{
+          this.isFav=favourits.some(fav=>fav.id == this.data.id)
+        },
+        error:(err)=>{
+          console.log(err);
+          
+        }
+      })
+    }
     toggleFav(){
       // this.isFav=!this.isFav
       if(!this.isFav){
@@ -31,14 +50,15 @@ export class ProductCardComponent {
         this.favoritesService.removeFavorite(this.data.id).subscribe({
           next:()=>{
             console.log(`${this.data.id} is removed`)
-            this.isFav=false
+            this.isFav=false;
+            this.removedFromFavorites.emit(this.data.id);
           },
           error:(err)=>{
               console.log(err);   
           }
         })
       }
-     }
-  
-   
+     }
+  
+   
 }
